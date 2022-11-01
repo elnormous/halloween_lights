3adaf0542f22088fb406e8d9ffa1f3ec9e4b08c5
Elviss Strazdins <elviss@elviss.lv>
2022-11-01 02:16:01 +0200

    First commit

---
+++ halloween_lights.ino
@@ -0,0 +1,55 @@
+enum class Mode
+{
+  Slow,
+  Fast
+};
+
+const float lowerValue = 0.1F;
+const float upperValue = 1.0F;
+const int cycles = 10;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(10, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+float getPulseValue(const float i)
+{
+  const float value = (sinf(i) + 1.0F) / 2.0F;
+  return (lowerValue + value * (upperValue - lowerValue));
+}
+
+void writeValue(const float i)
+{
+  const int value = static_cast<int>(i * 255.0F);
+  analogWrite(10, value);
+  analogWrite(13, value);
+}
+
+void loop()
+{
+  static float i = 0.0F;
+  static Mode mode = Mode::Slow;
+
+  i += 0.01F;
+
+  switch (mode)
+  {
+    case Mode::Slow:
+      writeValue(getPulseValue(i));
+      break;
+    case Mode::Fast:
+      writeValue(getPulseValue(i * 20.0F));
+      break;
+  }
+  
+  delay(10);
+
+  if (i >= static_cast<float>(cycles) * M_PI)
+  {
+    i = 0.0F;
+    mode = (mode == Mode::Slow) ? Mode::Fast : Mode::Slow;
+  }
+}
