65a512135ce38412f2989c3f74748648c939bee0
Elviss Strazdins <elviss@elviss.lv>
2022-11-01 02:37:15 +0200

    Use PI in getPulseValue

--- halloween_lights.ino
+++ halloween_lights.ino
@@ -1,55 +1,55 @@
 enum class Mode
 {
-  Slow,
-  Fast
+    Slow,
+    Fast
 };
 
 const float lowerValue = 0.1F;
 const float upperValue = 1.0F;
+const float speed = 0.002F;
 const int cycles = 5;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(10, OUTPUT);
-  pinMode(13, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(13, OUTPUT);
 }
 
 float getPulseValue(const float i)
 {
-  const float value = (sinf(i) + 1.0F) / 2.0F;
-  return (lowerValue + value * (upperValue - lowerValue));
+    const float value = (sinf(i * 2.0F * M_PI) + 1.0F) / 2.0F;
+    return (lowerValue + value * (upperValue - lowerValue));
 }
 
 void writeValue(const float i)
 {
-  const int value = static_cast<int>(i * 255.0F);
-  analogWrite(10, value);
-  analogWrite(13, value);
+    const int value = static_cast<int>(i * 255.0F);
+    analogWrite(10, value);
+    analogWrite(13, value);
 }
 
 void loop()
 {
-  static float i = 0.0F;
-  static Mode mode = Mode::Slow;
+    static float i = 0.0F;
+    static Mode mode = Mode::Slow;
 
-  i += 0.01F;
+    i += speed;
 
-  switch (mode)
-  {
-    case Mode::Slow:
-      writeValue(getPulseValue(i));
-      break;
-    case Mode::Fast:
-      writeValue(getPulseValue(i * 20.0F));
-      break;
-  }
-  
-  delay(10);
+    switch (mode)
+    {
+        case Mode::Slow:
+            writeValue(getPulseValue(i));
+            break;
+        case Mode::Fast:
+            writeValue(getPulseValue(i * 20.0F));
+            break;
+    }
 
-  if (i >= static_cast<float>(cycles) * 2.0F * M_PI)
-  {
-    i = 0.0F;
-    mode = (mode == Mode::Slow) ? Mode::Fast : Mode::Slow;
-  }
+    delay(10);
+
+    if (i >= static_cast<float>(cycles))
+    {
+        i = 0.0F;
+        mode = (mode == Mode::Slow) ? Mode::Fast : Mode::Slow;
+    }
 }
